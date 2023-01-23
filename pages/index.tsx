361c6bf5f5860753088d02c39db55e3bafa48aad
xdguido <guidogennari95@gmail.com>
2023-01-22 21:02:55 -0300

    Update Work header

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <section
                 id="hero"
                 className={clsx(
-                    'flex flex-col items-center text-center pt-[5rem] md:pt-[7rem] min-h-screen'
+                    'flex flex-col items-center text-center pt-[6rem] md:pt-[7rem] min-h-screen'
                 )}
             >
                 <h1 className="font-neue tracking-tight text-6xl md:text-7xl lg:text-8xl">
@@ -42,77 +42,76 @@ export default function Home() {
                     />
                 </div>
             </section>
-            <section
-                id="work"
-                className={clsx('relative flex flex-col justify-center items-center')}
-            >
-                <h2 className="absolute inset-x-0 -top-16 font-neue text-4xl text-blue-600">
-                    Work
-                </h2>
-                <div
-                    id="item-1"
-                    className="flex flex-col items-center mb-[6rem] lg:mb-[10rem] lg:flex-row lg:items-start"
-                >
-                    <div className="p-2 mb-3 lg:w-[26rem] lg:mr-12">
-                        <div className="text-xl font-medium mb-2 lg:text-2xl">Landing Pages</div>
-                        <div className=" text-gray-600 lg:text-lg">
-                            Modern and SEO optimized pages for business.
+            <section id="work" className="relative pt-[6rem] md:pt-[7rem]">
+                <h2 className="font-neue text-4xl text-blue-600 mb-8">Work</h2>
+                <div className="flex flex-col justify-center items-center">
+                    <div
+                        id="item-1"
+                        className="flex flex-col items-center mb-[6rem] lg:mb-[10rem] lg:flex-row lg:items-start"
+                    >
+                        <div className="p-2 mb-3 lg:w-[26rem] lg:mr-12">
+                            <div className="text-xl font-medium mb-2 lg:text-2xl">
+                                Landing Pages
+                            </div>
+                            <div className=" text-gray-600 lg:text-lg">
+                                Modern and SEO optimized pages for business.
+                            </div>
+                            <div className="flex justify-center md:justify-start mt-4 gap-6  ">
+                                <Button children="Live preview" intent="secondary" />
+                                <Button children="Details" intent="secondary" style="outline" />
+                            </div>
                         </div>
-                        <div className="flex mt-4 gap-6  ">
-                            <Button children="Live preview" intent="secondary" />
-                            <Button children="Details" intent="secondary" style="outline" />
+                        <div
+                            className={clsx(
+                                'relative w-[22rem] aspect-square',
+                                'md:w-[25rem] lg:w-[30rem] xl:w-[36rem]'
+                            )}
+                        >
+                            <Image
+                                className="object-cover rounded-lg shadow-md"
+                                src="/images/banner.png"
+                                alt="Portfolio Image"
+                                fill
+                                loading="lazy"
+                            />
                         </div>
                     </div>
                     <div
-                        className={clsx(
-                            'relative w-[22rem] aspect-square',
-                            'md:w-[25rem] lg:w-[30rem] xl:w-[36rem]'
-                        )}
+                        id="item-2"
+                        className="flex flex-col items-center mb-[6rem] lg:flex-row lg:items-start"
                     >
-                        <Image
-                            className="object-cover rounded-lg shadow-md"
-                            src="/images/banner.png"
-                            alt="Portfolio Image"
-                            fill
-                            loading="lazy"
-                        />
-                    </div>
-                </div>
-                <div
-                    id="item-2"
-                    className="flex flex-col items-center mb-[6rem] lg:flex-row lg:items-start"
-                >
-                    <div className="p-2 mb-3 lg:w-[26rem] lg:mr-12">
-                        <div className="text-xl font-medium mb-2 lg:text-2xl">
-                            Full-stack Library App
+                        <div className="p-2 mb-3 lg:w-[26rem] lg:mr-12">
+                            <div className="text-xl font-medium mb-2 lg:text-2xl">
+                                Full-stack Library App
+                            </div>
+                            <div className=" text-gray-600 lg:text-lg">
+                                Build and share your favourites book lists.
+                            </div>
+                            <div className="flex justify-center md:justify-start mt-4 gap-6  ">
+                                <Button children="Live preview" intent="secondary" />
+                                <Button children="Details" intent="secondary" style="outline" />
+                            </div>
                         </div>
-                        <div className=" text-gray-600 lg:text-lg">
-                            Build and share your favourites book lists.
-                        </div>
-                        <div className="flex mt-4 gap-6  ">
-                            <Button children="Live preview" intent="secondary" />
-                            <Button children="Details" intent="secondary" style="outline" />
+                        <div
+                            className={clsx(
+                                'relative w-[22rem] aspect-square',
+                                'md:w-[25rem] lg:w-[30rem] xl:w-[36rem]'
+                            )}
+                        >
+                            <Image
+                                className="object-cover rounded-lg shadow-md"
+                                src="/images/banner2.png"
+                                alt="Portfolio Image"
+                                fill
+                                loading="lazy"
+                            />
                         </div>
                     </div>
-                    <div
-                        className={clsx(
-                            'relative w-[22rem] aspect-square',
-                            'md:w-[25rem] lg:w-[30rem] xl:w-[36rem]'
-                        )}
-                    >
-                        <Image
-                            className="object-cover rounded-lg shadow-md"
-                            src="/images/banner2.png"
-                            alt="Portfolio Image"
-                            fill
-                            loading="lazy"
-                        />
+                    <div>
+                        <button className="flex items-center gap-3 text-lg font-semibold text-gray-600 hover:text-blue-600 focus-visible:text-blue-600 active:text-blue-800 p-2">
+                            More projects <FaArrowCircleRight className="" />
+                        </button>
                     </div>
-                </div>
-                <div>
-                    <button className="flex items-center gap-3 text-lg font-semibold text-gray-600 hover:text-blue-600 focus-visible:text-blue-600 active:text-blue-800 p-2">
-                        More projects <FaArrowCircleRight className="" />
-                    </button>
                 </div>
             </section>
         </Layout>
