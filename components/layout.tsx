f7c03c065e3a24f0d7ea778ac26e0b5d92be1b28
xdguido <guidogennari95@gmail.com>
2023-02-13 20:53:16 -0300

    Update styling

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,73 +1,11 @@
-import { FaGithub, FaLinkedinIn, FaDiscord } from 'react-icons/fa';
-import ThemeToggler from '@ui/ThemeToggler';
+import Header from './header';
 
 export const siteTitle = 'Guido Gennari - Web Development';
 
 export default function Layout({ children, home }: { children: React.ReactNode; home?: boolean }) {
     return (
         <div className="flex flex-col min-h-screen bg-slate-50 dark:bg-black dark:text-gray-100">
-            <header className="z-50 fixed inset-x-0 top-0 flex flex-1 items-center justify-between backdrop-blur-md border-b border-b-gray-300 dark:border-b-gray-500 p-4 lg:p-5">
-                <h1 className="font-neue leading-6 tracking-tight text-xl">
-                    Guido
-                    <br />
-                    Gennari
-                </h1>
-                <div className="flex gap-3">
-                    <ul id="socials" className="font-extrabold tracking-tight text-3xl flex">
-                        <li>
-                            <a
-                                title="GitHub"
-                                target="_blank"
-                                href="https://github.com/xdguido"
-                                rel="noopener noreferrer"
-                            >
-                                <span className="sr-only">GitHub page</span>
-                                <div
-                                    className="rounded hover:text-blue-600 focus-visible:text-blue-600 active:text-blue-600  p-1"
-                                    aria-hidden="true"
-                                >
-                                    <FaGithub />
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                title="Discord"
-                                target="_blank"
-                                href="https://discordapp.com/users/xdguido#4212"
-                                rel="noopener noreferrer"
-                            >
-                                <span className="sr-only">Discord profile</span>
-                                <div
-                                    className="rounded hover:text-blue-600 focus-visible:text-blue-600 active:text-blue-600  p-1"
-                                    aria-hidden="true"
-                                >
-                                    <FaDiscord />
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                title="Linkedin"
-                                target="_blank"
-                                href="https://www.linkedin.com/in/guido-gennari/"
-                                rel="noopener noreferrer"
-                            >
-                                <span className="sr-only">Linkedin page</span>
-                                <div
-                                    className="rounded hover:text-blue-600 focus-visible:text-blue-600 active:text-blue-600  p-1"
-                                    aria-hidden="true"
-                                >
-                                    <FaLinkedinIn />
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                    <div className="flex justify-center items-center pl-5 ml-5 border-l border-l-slate-200 dark:border-l-slate-400">
-                        <ThemeToggler />
-                    </div>
-                </div>
-            </header>
+            <Header />
             <main className="flex flex-auto flex-col">{children}</main>
             <footer className="flex items-center justify-between bg-black dark:bg-gray-300 h-[8rem] p-4 lg:p-5">
                 <h1 className="font-neue text-white dark:text-gray-900 leading-6 tracking-tight text-xl">
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -12,7 +12,7 @@ const buttonStyles = cva(
             colorScheme: {
                 blue: ['text-blue-600'],
                 gray: ['text-gray-500'],
-                black: ['text-black']
+                black: ['text-inherit']
             },
             style: {
                 solid: ['shadow-md text-gray-50', 'hover:shadow-lg', 'focus-visible:shadow-lg'],
@@ -28,8 +28,6 @@ const buttonStyles = cva(
             style: 'solid'
         },
         compoundVariants: [
-            // Applied via:
-            //   `button({ colorScheme: "blue", size: "medium" })`
             {
                 colorScheme: 'blue',
                 style: 'solid',
@@ -47,7 +45,7 @@ const buttonStyles = cva(
             {
                 colorScheme: 'gray',
                 style: 'solid',
-                class: ['bg-gray-500', 'hover:bg-gray-500 ', 'focus-visible:bg-gray-500']
+                class: ['bg-gray-500', 'hover:bg-gray-400 ', 'focus-visible:bg-gray-500']
             },
             {
                 colorScheme: 'gray',
@@ -55,21 +53,21 @@ const buttonStyles = cva(
                 class: [
                     'border-gray-500 text-gray-500',
                     'hover:border-gray-400 hover:text-gray-400',
-                    'focus-visible:border-gray-500 focus-visible:text-gray-500'
+                    'focus-visible:border-gray-400 focus-visible:text-gray-400'
                 ]
             },
             {
                 colorScheme: 'black',
                 style: 'solid',
-                class: ['bg-black', 'hover:bg-blue-600 ', 'focus-visible:bg-blue-600']
+                class: ['bg-black', 'hover:bg-blue-500 ', 'focus-visible:bg-blue-500']
             },
             {
                 colorScheme: 'black',
                 style: 'outline',
                 class: [
                     'border-black text-black',
-                    'hover:border-blue-600 hover:text-blue-600',
-                    'focus-visible:border-blue-600 focus-visible:text-blue-600'
+                    'hover:border-blue-500 hover:text-blue-500',
+                    'focus-visible:border-blue-500 focus-visible:text-blue-500'
                 ]
             }
         ]
@@ -80,6 +78,9 @@ interface ButtonProps {
     href?: string;
     children: React.ReactNode;
     onClick?: () => void;
+    title?: string;
+    target?: string;
+    rel?: string;
 }
 
 export interface Props extends ButtonProps, VariantProps<typeof buttonStyles> {}
