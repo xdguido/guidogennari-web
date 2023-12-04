c7fff10d774d51d3d4ac73acfb2db608256cad7c
xdguido <guidogennari95@gmail.com>
2023-12-04 17:27:32 -0300

    feat: update resume content and style

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -8,20 +8,20 @@ export default function Resume() {
             <NextSeo title="Guido Gennari | Resume" />
             <div
                 id="resume"
-                className="relative flex min-h-screen flex-col gap-20 pt-[12rem] print:gap-9 print:pt-8 print:text-black md:pt-[8rem] lg:gap-28 lg:pt-[13rem]"
+                className="relative flex min-h-screen flex-col gap-20 pt-[12rem] print:gap-2 print:pt-8 print:text-black md:pt-[8rem] lg:gap-24 lg:pt-[13rem]"
             >
                 <div className="flex flex-col gap-3">
                     <h1 className="items-baseline gap-x-10 lg:grid lg:grid-cols-4">
-                        <span className="-ml-1 text-right  text-5xl tracking-tighter print:text-7xl sm:text-7xl">
+                        <span className="-ml-1 text-right  text-5xl tracking-tighter print:text-xl sm:text-7xl">
                             GUIDO
                         </span>
-                        <span className="text-5xl font-bold tracking-tighter print:text-7xl sm:text-7xl lg:-ml-2">
+                        <span className="text-5xl font-bold tracking-tighter print:text-xl sm:text-7xl lg:-ml-2">
                             GENNARI
                         </span>
                     </h1>
                     <div className="items-baseline gap-x-10 lg:grid lg:grid-cols-4">
                         <h2 className="col-span-2 col-start-2 text-xl tracking-tight text-gray-400">
-                            Full-stack web developer
+                            Frontend web developer
                         </h2>
                         <div className="col-start-4 ml-auto">
                             <Button
@@ -35,51 +35,58 @@ export default function Resume() {
                     </div>
                 </div>
                 <section id="about" className="flex flex-col gap-6 print:gap-4">
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <h3 className="text-2xl font-semibold text-blue-600 print:col-start-2 lg:col-start-2">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <h3 className="text-2xl font-semibold text-blue-600 print:text-base lg:col-start-2">
                             Summary
                         </h3>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <p className="mb-2 text-justify print:col-span-3 print:col-start-2 sm:text-sm lg:col-span-3 lg:col-start-2">
-                            I{"'"}m a{' '}
-                            <span className="font-semibold">full-stack web developer</span> with
-                            expertise in building web sites and web applications with React. I{"'"}m
-                            currently looking for opportunities to work with a team where I can
-                            contribute my skills and work with others to create
-                            <span className="font-semibold"> digital products.</span>
-                        </p>
-                        <p className="text-justify print:col-span-3 print:col-start-2 sm:text-sm lg:col-span-3 lg:col-start-2">
-                            If you're looking for a team player who is dedicated to delivering code
-                            and adapt to new challenges, I'd love to hear from you. Please don't
-                            hesitate to get in touch if you think I could be a good fit for your
-                            team.
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <p className="mb-2 text-justify print:col-span-3 sm:text-sm lg:col-span-3 lg:col-start-2">
+                            I{"'"}m a frontend web developer with expertise in building web sites
+                            and web applications, currently working with React.
                         </p>
                     </div>
                 </section>
                 <section id="experience" className="flex flex-col gap-6 print:gap-4">
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <h3 className="text-2xl font-semibold text-blue-600 print:col-start-2 lg:col-start-2">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <h3 className="text-2xl font-semibold text-blue-600 print:text-base lg:col-start-2">
                             Experience
                         </h3>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <div className="print:text-right lg:text-right">
-                            <span className="text-sm text-gray-400">May 2023 - Present</span>
-                            <br />
-                            <span className="text-sm text-gray-400">Self-employed</span>
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <div className=" lg:text-right">
+                            <span className="text-sm text-gray-400">Aug 2023 - Present</span>
                         </div>
                         <div className="print:col-span-3 lg:col-span-3">
                             <h2 className="mb-2 ">
-                                <span className=" text-lg font-semibold">
-                                    Full-stack web developer
+                                <span className="font-semibold">
+                                    Frontend developer at ClimateScience
                                 </span>
                             </h2>
+                            <ul className="ml-5  list-disc sm:text-sm">
+                                <li className="mb-1 text-justify">
+                                    Write components libraries for different products using Radix
+                                    primitives.
+                                </li>
+                                <li className="mb-1 text-justify">
+                                    Develop React components following the design guidelines,
+                                    ensuring proper UX and accessibility.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <div className=" lg:text-right">
+                            <span className="text-sm text-gray-400">May 2023 - Aug 2023</span>
+                        </div>
+                        <div className="print:col-span-3 lg:col-span-3">
+                            <h2 className="mb-2 ">
+                                <span className=" font-semibold">Fullstack developer</span>
+                            </h2>
                             <p className="mb-2 text-justify sm:text-sm">
-                                Developing a fully-functional e-commerce web app for a Furniture
-                                Factory. The application will feature an easy-to-use interface where
-                                customers can browse products, add items to their shopping cart, and
-                                complete their purchase securely.
+                                Developing a e-commerce app. The application will feature an
+                                easy-to-use interface where customers can browse products, add items
+                                to their shopping cart, and complete their purchase securely.
                             </p>
                             <ul className="ml-5 list-disc sm:text-sm">
                                 <li className="mb-1 text-justify">
@@ -90,51 +97,22 @@ export default function Resume() {
                                     Designed and developed Backend and Data Base using Next.js API
                                     Routes, Prisma and PosgreSQL.
                                 </li>
-                                <li className="mb-1">Deployment with Vercel and PlanetScale.</li>
                                 <li className="mb-1">
                                     Integrated User Authentication and Purchase Order Gateways.
-                                </li>
-                                <li className="mb-1">
-                                    Optimized the website for search engines and accessibility
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <div className="print:text-right lg:text-right">
-                            <span className="text-sm text-gray-400">Feb 2023 - Present</span>
-                            <br />
-                            <span className="text-sm text-gray-400">Remote contractor</span>
-                        </div>
-                        <div className="print:col-span-3 lg:col-span-3">
-                            <h2 className="mb-2 ">
-                                <span className="font-semibold">Front-end web developer </span>
-                            </h2>
-                            <ul className="ml-5  list-disc sm:text-sm">
-                                <li className="mb-1 text-justify">
-                                    Developed a portfolio of landing websites for clients on UpWork,
-                                    with a focus on improving conversion rates and lead generation.
-                                </li>
-                                <li className="mb-1 text-justify">
-                                    Utilized Next.js to build high-quality, responsive landing
-                                    websites that were optimized for search engines and social media
-                                    sharing.
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </section>
                 <section id="education" className="flex flex-col gap-6 print:gap-4 ">
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <h3 className="text-2xl font-semibold text-blue-600 print:col-start-2 lg:col-start-2">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <h3 className="text-2xl font-semibold text-blue-600 print:text-base lg:col-start-2">
                             Education
                         </h3>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <div className="print:text-right lg:text-right">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <div className=" lg:text-right">
                             <span className="text-sm text-gray-400">Mar 2023 - Present</span>
-                            <br />
-                            <span className="text-sm text-gray-400">Remote</span>
                         </div>
                         <div className="print:col-span-3 lg:col-span-3">
                             <h2 className="mb-2 text-lg sm:text-base">
@@ -154,15 +132,11 @@ export default function Resume() {
                                     tools and practices, and deliver a working product that provides
                                     value to the community.
                                 </li>
-                                <li className="mb-1 text-justify">
-                                    Stand out to potential employers and increase chances of getting
-                                    hired in the IT job market.
-                                </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <div className="print:text-right lg:text-right">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <div className=" lg:text-right">
                             <span className="text-sm text-gray-400">Dec 2021 - Present</span>
                         </div>
                         <div className="print:col-span-3 lg:col-span-3">
@@ -172,27 +146,23 @@ export default function Resume() {
                             </h2>
                             <p className="mb-2 text-justify sm:text-sm">
                                 I started studying at open-source academies such as freeCodeCamp,
-                                where I learned the basics. I continued with more advanced tutorials
-                                and code challenges, gaining experience in building production-grade
-                                software through this process.
+                                where I learned the basics. I continued with more code challenges,
+                                gaining experience in building production-grade software through
+                                this process.
                             </p>
                             <ul className="ml-5  list-disc sm:text-sm">
                                 <li className="mb-1">
                                     Software development environment and version control
                                 </li>
                                 <li className="mb-1">JavaScript Algorithms and Data Scructures</li>
-                                <li className="mb-1">Front end development</li>
-                                <li className="mb-1">Back end development</li>
-                                <li className="mb-1">Relational Databases</li>
-                                <li className="mb-1">Non-Relational Databases</li>
+                                <li className="mb-1">Fullstack web development</li>
+                                <li className="mb-1">Databases</li>
                             </ul>
                         </div>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <div className="print:text-right lg:text-right">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <div className=" lg:text-right">
                             <span className="text-sm text-gray-400">Mar 2015 - Dic 2020</span>
-                            <br />
-                            <span className="text-sm text-gray-400">Neuquén city</span>
                         </div>
                         <div className="print:col-span-3 lg:col-span-3">
                             <h2 className="mb-2 text-lg sm:text-base">
@@ -213,11 +183,9 @@ export default function Resume() {
                             </ul>
                         </div>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <div className="print:text-right lg:text-right">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <div className=" lg:text-right">
                             <span className="text-sm text-gray-400">Mar 2011 - Mar 2015</span>
-                            <br />
-                            <span className="text-sm text-gray-400">Neuquén city</span>
                         </div>
                         <div className="print:col-span-3 lg:col-span-3">
                             <h2 className="mb-2 text-lg sm:text-base">
@@ -232,13 +200,13 @@ export default function Resume() {
                     </div>
                 </section>
                 <section id="stack" className="flex flex-col gap-6 print:gap-4">
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <h3 className="text-2xl font-semibold text-blue-600 print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <h3 className="text-2xl font-semibold text-blue-600 print:col-span-3 print:text-base lg:col-span-3 lg:col-start-2">
                             Skills and Tech Stack
                         </h3>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <div className="print:col-span-3 lg:col-span-3 lg:col-start-2">
                             <h2 className="my-2 font-bold">Tech Stack:</h2>
                             <ul className="ml-5 list-disc sm:text-sm ">
                                 <li className="mb-1">JavaScript / TypeScript</li>
@@ -266,13 +234,13 @@ export default function Resume() {
                     </div>
                 </section>
                 <section id="contact" className="flex flex-col gap-6 print:gap-4">
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <h3 className="text-2xl font-semibold text-blue-600 print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <h3 className="text-2xl font-semibold text-blue-600 print:col-span-3 print:text-base lg:col-span-3 lg:col-start-2">
                             Contact
                         </h3>
                     </div>
-                    <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-                        <ul className="ml-5 list-disc print:col-span-3 print:col-start-2 sm:text-sm lg:col-span-3 lg:col-start-2">
+                    <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                        <ul className="ml-5 list-disc print:col-span-3 sm:text-sm lg:col-span-3 lg:col-start-2">
                             <li className="mb-1">
                                 Personal Website:{' '}
                                 <a
