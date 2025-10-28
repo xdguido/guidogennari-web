9eb870aae43054aa6bc3ffcf432451df1d1f1908
xdguido <guidogennari95@gmail.com>
2025-10-28 20:57:12 -0300

    feat: update content

--- lib/about/en.ts
+++ lib/about/en.ts
@@ -1,7 +1,7 @@
 const en = {
   title: "Hey, I'm Guido! 👋",
   paragraph1:
-    "I'm a web developer based in Neuquén, Argentina. I specialize in web and mobile applications with React and Next.js. Currently, I work on B2B freelance projects, including logistics applications, budgeting systems for the furniture industry, among others. I collaborate with clients to create intuitive user interfaces, integrate APIs for seamless user experiences, and maintain accessibility standards.",
+    "I'm a web developer based in Neuquén, Argentina. I specialize in web and mobile applications with React and Next.js. Currently, I work on B2B and B2C freelance projects, including logistics applications, budgeting systems for the furniture industry, among others. I collaborate with clients to create intuitive user interfaces, integrate APIs for seamless user experiences, and maintain accessibility standards.",
   paragraph2:
     "I have a wide range of interests, from web development and game design to photography and mountaineering. I've always been interested in technology and computers. During recent years, I've specialized in modern web development, spending significant time building robust web and mobile applications.",
   paragraph3:
--- lib/about/es.ts
+++ lib/about/es.ts
@@ -1,7 +1,7 @@
 const es = {
   title: '¡Hola! Soy Guido 👋',
   paragraph1:
-    'Soy un desarrollador web con sede en Neuquén, Argentina. Me especializo en aplicaciones web y móviles con React y Next.js. Actualmente trabajo en proyectos freelance B2B, incluyendo aplicaciones de logística, sistemas de presupuestado para la industria del amoblamiento, entre otros. Colaboro con clientes para crear interfaces de usuario intuitivas, integrar APIs para experiencias de usuario perfectas y mantener estándares de accesibilidad.',
+    'Soy un desarrollador web con sede en Neuquén, Argentina. Me especializo en aplicaciones web y móviles con React y Next.js. Actualmente trabajo en proyectos freelance B2B y B2C, incluyendo aplicaciones de logística, sistemas de presupuestado para la industria del amoblamiento, entre otros. Colaboro con clientes para crear interfaces de usuario intuitivas, integrar APIs para experiencias de usuario perfectas y mantener estándares de accesibilidad.',
   paragraph2:
     'Tengo una amplia gama de intereses, desde el desarrollo web y el diseño de juegos hasta la fotografía y el montañismo. Siempre me ha interesado la tecnología y las computadoras. Durante los últimos años, me he especializado en desarrollo web moderno, dedicando tiempo a la construcción de aplicaciones web y móviles robustas.',
   paragraph3:
